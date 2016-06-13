345fc9d5e195739ad16f09eb571b029dc39a7406
Victor Nate Graf <nategraf@verizon.net>
2016-06-12 22:11:44 -0500

    Reformaatted checkers page begun

---
+++ assets/js/checkers_slider.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function ($) {
+
+    var jssor_1_options = {
+        $AutoPlay: true,
+        $Idle: 5000,
+        $CaptionSliderOptions: {
+            $Class: $JssorCaptionSlideo$,
+            $Transitions: [],
+            $Breaks: [
+                [{ d: 2000, b: 1000 }]
+            ]
+        },
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+        },
+        $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$
+        }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    //responsive code begin
+    //you can remove responsive code if you don't want the slider scales while window resizing
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 1280);
+            jssor_1_slider.$ScaleWidth(refSize);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    //responsive code end
+});
